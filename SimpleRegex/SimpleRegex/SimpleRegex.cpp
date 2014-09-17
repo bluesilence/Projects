4c911c83a7ff72f3642fba2526535314f783c4c3
Bluesilence <bluesilencemail@126.com>
2014-09-18 07:40:43 +0800

    Simple Regex

---
+++ SimpleRegex/SimpleRegex/SimpleRegex.cpp
@@ -0,0 +1,69 @@
+#include "SimpleRegex.h"
+using namespace std;
+
+int match_star(char *regexp, char* text) {
+	char c = regexp[0];
+
+	regexp += 2;	//Move to the char behind *
+    do {
+        if(match_here(regexp, text) == 1)	//Move along the text as matched in *, until match succeeded
+			return 1;        
+    }
+    while((*text) != '\0' && (*text++ == c || c == '.'));
+
+    return 0;
+}
+
+int match_plus(char *regexp, char* text) {
+	if( !regexp || !text )
+		throw exception("NullPointerException");
+	
+	char c = regexp[0];
+	if( c != '.' && c != '*' && c != *text++ )
+		return 0;
+
+	//Match once succeeded, match for the rest
+	regexp += 2;
+    do {
+        if(match_here(regexp, text) == 1)
+			return 1;        
+    }
+    while((*text) != '\0' && (*text++ == c || c == '.'));
+
+    return 0;
+}
+
+int match_here(char *regexp, char* text) {
+    if(regexp[0] == '\0')
+		return 1;	//Match succeeded
+    if(regexp[1] == '+')
+		return match_plus(regexp, text);
+    if(regexp[1] == '*')
+		return match_star(regexp, text);
+	if(regexp[0] == '^')
+		return match_here(regexp + 1, text);
+    if(regexp[0] == '$')
+		return text[0] == '\0';
+    if(regexp[0] == '.' && text[0] != '\0')
+		return match_here(regexp + 1, text + 1);
+    if(regexp[0] == text[0] && text[0] != '\0')
+		return match_here(regexp + 1, text + 1);
+
+    return 0;	//Match failed
+
+}
+
+int main() { 
+  char *pattern = "^a*b+c$";
+  char *successText1 = "aabc";
+  char *failureText1 = "aab";
+  char *successText2 = "bc";
+  char *failureText2 = "bac";
+  cout<<pattern<<" ON "<<successText1<<": "<<match_here(pattern, successText1)<<endl;
+  cout<<pattern<<" ON "<<failureText2<<": "<<match_here(pattern, failureText2)<<endl;
+  cout<<pattern<<" ON "<<successText1<<": "<<match_here(pattern, successText1)<<endl;
+  cout<<pattern<<" ON "<<failureText2<<": "<<match_here(pattern, failureText2)<<endl;
+
+  getchar();
+  return 0; 
+} 
---
+++ SimpleRegex/SimpleRegex/SimpleRegex.h
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+#include <iostream>
+
+int match_star(int c, char *regexp, char*text);
+
+int match_here(char *regexp, char* text);
